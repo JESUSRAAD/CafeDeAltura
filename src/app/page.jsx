7a0e22d5dc4f87d73bdd4abcf1c97bdc97fcc0e4
Jesus Raad <jesus.raad@students.gammatech.school>
2024-08-07 18:28:40 +0200

    new page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,21 @@
-import Form from '@/components/Form'
-import UserList from '@/components/UserList'
-import React from 'react'
+// import Form from '@/components/Form'
+// import UserList from '@/components/UserList'
+// import React from 'react'
 
-const HomePage = () => {
-  return (
-<div>
+// const HomePage = () => {
+//   return (
+// <div>
 
-<div className='flex justify-center items-center h-screen'><Form/></div>
-<div className='flex justify-center h-screen'><UserList/></div>
+// <div className='flex justify-center items-center h-screen'><Form/></div>
+// <div className='flex justify-center h-screen'><UserList/></div>
 
-</div>
-
-
+// </div>
 
 
 
-  )
-}
 
-export default HomePage
+
+//   )
+// }
+
+// export default HomePage
